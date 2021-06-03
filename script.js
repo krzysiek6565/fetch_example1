01923de414c294579f02091a7a77de4c63e9c557
krzysiek <kkos8@wp.pl>
2021-06-03 23:22:59 +0200

    Updating infroamtion about promises

--- script.js
+++ script.js
@@ -31,6 +31,20 @@ Promises - shordhand functions
 ********************************************************************************
 
 chooseToppings()
+.then(function(toppings) {
+  return placeOrder(toppings);
+})
+.then(function(order) {
+  return collectOrder(order);
+})
+.then(function(pizza) {
+  eatPizza(pizza);
+})
+.catch(failureCallback);
+
+********************************************************************************
+
+chooseToppings()
 .then(toppings =>
   placeOrder(toppings)
 )
